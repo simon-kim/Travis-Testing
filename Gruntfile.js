4efca1f7cb51b1aa5c35737a1920c7cddffa6336
Simon Kim <simon.ty.kim@gmail.com>
2014-11-11 22:12:33 -0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-simple-mocha");
-  grunt.loadNpmTasks("grunt-jscs");
 
   grunt.initConfig({
     jshint: {
